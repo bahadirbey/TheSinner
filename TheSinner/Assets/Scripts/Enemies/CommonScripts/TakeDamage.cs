c5212370f5fc1cf958521649c6bf1ade659ccae9
bahadirbey <zubeyrbahadir@gmail.com>
2020-10-29 13:47:00 +0300

    PlayerAttack-1

---
+++ TheSinner/Assets/Scripts/Enemies/CommonScripts/TakeDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamage : MonoBehaviour
+{
+    public int health;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+    } 
+
+    public void GetDamage(int damage)
+    {
+        health -= damage;
+        animator.SetTrigger("getHit");
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- TheSinner/Assets/Scripts/Player/PlayerMovement.cs
+++ TheSinner/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //General elements Begin
+    public static PlayerMovement instance;
     private Rigidbody2D rb;
     private Animator animator;
     //General elements End
@@ -27,8 +28,39 @@ public class PlayerMovement : MonoBehaviour
 
     private bool extraJump;
     //Jump End
+
+    //Roll Begin
+    private float rollingSpeed;
+    public float slindingTime;
+    //Roll End
+
+    //Melee attack Begin
+    public float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask whatIsEnemies;
+
+    public int damage;
+
+    public int noOfClicks = 0;
+    float lastClickedTime;
+    public float maxComboDelay;
+
+    bool attacking;
+    //Melee attack End
+
+    private State state;
+    enum State
+    {
+        Normal,
+        DodgeRollSliding,
+    }
+
     void Start()
     {
+        state = State.Normal;
         checkRadius = .1f;
         facingRight = true;
         rb = GetComponent<Rigidbody2D>();
@@ -39,12 +71,27 @@ public class PlayerMovement : MonoBehaviour
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
         horizontalMove = Input.GetAxis("Horizontal");
-        Animate();
-        Move();
-        Flip();
-        Jump();
-    }
 
+        if (Time.time - lastClickedTime > maxComboDelay)
+        {
+            noOfClicks = 0;
+        }
+
+        switch (state)
+        {
+            case State.Normal:
+                Animate();
+                Move();
+                Flip();
+                Jump();
+                Roll();
+                MeleeAttack();
+                break;
+            case State.DodgeRollSliding:
+                RollSliding();
+                break;
+        }
+    }
     void Animate()
     {
         animator.SetFloat("horizontalSpeed", Mathf.Abs(horizontalMove));
@@ -61,7 +108,8 @@ public class PlayerMovement : MonoBehaviour
         {
             facingRight = false;
             transform.eulerAngles = new Vector3(0, 180, 0);
-        }else if (!facingRight && horizontalMove > 0)
+        }
+        else if (!facingRight && horizontalMove > 0)
         {
             facingRight = true;
             transform.eulerAngles = new Vector3(0, 0, 0);
@@ -74,10 +122,110 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
             extraJump = true;
-        }else if(Input.GetKeyDown(KeyCode.Space) && !isGrounded && extraJump)
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && !isGrounded && extraJump)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
             extraJump = false;
         }
+    }
+
+
+    void Roll()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && isGrounded)
+        {
+            state = State.DodgeRollSliding;
+            rollingSpeed = 1600f;
+            animator.SetTrigger("roll");
+        }
+    }
+
+    void RollSliding()
+    {
+        if (facingRight)
+        {
+            rb.velocity = new Vector2(rollingSpeed * Time.deltaTime, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-rollingSpeed * Time.deltaTime, rb.velocity.y);
+        }
+
+        rollingSpeed -= rollingSpeed * slindingTime * Time.deltaTime;
+
+        if (rollingSpeed < 200f)
+        {
+            state = State.Normal;
+        }
+    }
+
+    void MeleeAttack()
+    {
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                lastClickedTime = Time.time;
+                noOfClicks++;
+                if (noOfClicks == 1)
+                {
+                    animator.SetBool("attack1", true);
+                }
+                noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
+
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<TakeDamage>().GetDamage(damage);
+                }
+
+                timeBtwAttack = startTimeBtwAttack;
+            }
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+
+    public void returnFirst()
+    {
+        if (noOfClicks >= 2)
+        {
+            animator.SetBool("attack2", true);
+        }
+        else
+        {
+            animator.SetBool("attack1", false);
+            noOfClicks = 0;
+        }
+    }
+
+    public void returnSecond()
+    {
+        if (noOfClicks >= 3)
+        {
+            animator.SetBool("attack3", true);
+        }
+        else
+        {
+            animator.SetBool("attack2", false);
+            noOfClicks = 0;
+        }
+    }
+
+    public void returnLast()
+    {
+        animator.SetBool("attack1", false);
+        animator.SetBool("attack2", false);
+        animator.SetBool("attack3", false);
+        noOfClicks = 0;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
     }
 }
