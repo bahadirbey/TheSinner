9508dc58ba2ae3ae9ab81cef59cc4b44bf64d4dc
ahmet6161569 <48096413+ahmet6161569@users.noreply.github.com>
2020-11-03 15:48:15 +0300

    Envanter-Equip kısmı devam edilicek

---
+++ TheSinner/Assets/Scripts/Envanter/DragHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField] private Canvas canvas;
+
+
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Tıkladın");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Sürükleniyor");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("Bıraktın");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
