165d0ee467ebc82fa0d42594b2f8ea554b4bb158
ahmet6161569 <48096413+ahmet6161569@users.noreply.github.com>
2020-11-17 19:18:04 +0300

    componion itemi yapıldı
    market-companionuda yapıldı

--- TheSinner/Assets/Scripts/Market/MarketTest/BoneMarket.cs
+++ TheSinner/Assets/Scripts/Market/MarketTest/BoneMarket.cs
@@ -2,31 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class redballmarket : MonoBehaviour
+public class BoneMarket : MonoBehaviour
 {
-    
-    public GameObject item1infos;
-    
+
+    public GameObject item2infos;
+
     // Start is called before the first frame update
 
     public void Update()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            item1infos.gameObject.SetActive(true);
+            item2infos.gameObject.SetActive(true);
         }
-        
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
-            item1infos.gameObject.SetActive(false);
+            item2infos.gameObject.SetActive(false);
         }
     }
 }
