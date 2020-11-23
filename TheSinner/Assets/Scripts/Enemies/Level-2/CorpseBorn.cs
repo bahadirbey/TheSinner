b8ffd1295a778f303d693b7cc6212d6c655f6079
bahadirbey <zubeyrbahadir@gmail.com>
2020-11-23 14:28:20 +0300

    SkeletonSwordAdded

---
+++ TheSinner/Assets/Scripts/Enemies/Level-2/CorpseBorn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorpseBorn : MonoBehaviour
+{
+    private GameObject player;
+    private Animator animator;
+    public GameObject skeleton;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        BornController();
+    }
+
+    void BornController()
+    {
+        if (Vector2.Distance(player.transform.position, transform.position) < 2f)
+        {
+            animator.SetTrigger("born");
+        }
+    }
+
+    public void Born()
+    {
+        Instantiate(skeleton, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
