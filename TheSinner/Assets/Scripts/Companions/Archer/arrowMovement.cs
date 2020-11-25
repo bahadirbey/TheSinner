97b0ff86ea88894cbc359ad1b752a328a8afeacd
bahadirbey <zubeyrbahadir@gmail.com>
2020-11-26 02:01:28 +0300

    archer's arrow fixed

--- TheSinner/Assets/Scripts/Companions/Archer/arrowMovement.cs
+++ TheSinner/Assets/Scripts/Companions/Archer/arrowMovement.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 public class arrowMovement : MonoBehaviour
 {
     public float speed;
-    public GameObject destroyAnimation;
     public int damage;
-    
+    private Vector2 targetPos;
+    public GameObject explosionEffect;
+    public float attackRange;
+    public LayerMask whatIsEnemy;
+    Collider2D[] enemiesToDamage;
+
+    private void Start()
+    {
+        if (ArcherManager.facingRight)
+        {
+            targetPos = new Vector3(transform.position.x + 10, transform.position.y, 0);
+        }
+        else
+        {
+            targetPos = new Vector3(transform.position.x - 10, transform.position.y, 0);
+        }
+        
+    }
+
     void Update()
     {
+        enemiesToDamage = Physics2D.OverlapCircleAll(transform.position, attackRange, whatIsEnemy);
         Move();
     }
 
@@ -18,22 +36,41 @@ public class arrowMovement : MonoBehaviour
         if (ArcherManager.facingRight)
         {
             transform.eulerAngles = new Vector3(0,0,-135);
-            transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
         }
         else
         {
             transform.eulerAngles = new Vector3(0, 0, 45);
-            transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
         }
+
+        if (Vector2.Distance(transform.position, targetPos) < .2f)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, Quaternion.identity); 
+        for (int i = 0; i < enemiesToDamage.Length; i++)
+        {
+            enemiesToDamage[i].GetComponent<TakeDamage>().GetDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
+        if (collision.gameObject.tag == "Enemy")
         {
             collision.gameObject.GetComponent<TakeDamage>().GetDamage(damage);
-            Instantiate(destroyAnimation, transform.position, Quaternion.identity);
-            Destroy(gameObject);
         }
     }
 }
