8cc18410248f25f7f61c21a9aedfaef2e1de1087
bahadirbey <68642072+bahadirbey@users.noreply.github.com>
2021-02-03 18:00:16 +0300

    Crafting Coding Part-1

---
+++ TheSinner/Assets/Scripts/Market/InventoryTestScripts/Crafting/SelectCraftStone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCraftStone : MonoBehaviour
+{
+    private CraftingSlots craftItems;
+    public GameObject itemButton;
+    public GameObject item;
+
+    public string stoneName;
+    public int level;
+
+    void Start()
+    {
+        craftItems = GameObject.FindGameObjectWithTag("Player").GetComponent<CraftingSlots>();
+    }
+
+    public void Craft()
+    {
+        for (int i = 0; i < craftItems.slots.Length; i++)
+        {
+            if (craftItems.isFull[i] == false)
+            {
+                Instantiate(itemButton, craftItems.slots[i].transform, false);
+                craftItems.isFull[i] = true;
+                Destroy(item);
+                break;
+            }
+        }
+    }
+}
---
+++ TheSinner/Assets/Scripts/Market/InventoryTestScripts/Crafting/SpawnCrafting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCrafting : MonoBehaviour
+{
+    public GameObject item;
+    private InventoryTest inventoryTest;
+
+    public int whichStone;
+
+    void Start()
+    {
+        inventoryTest = GameObject.FindGameObjectWithTag("Player").GetComponent<InventoryTest>();
+    }
+
+    public void RemoveItem()
+    {
+        for (int i = 0; i < inventoryTest.slots.Length; i++)
+        {
+            if (inventoryTest.isFull[i] == false)
+            {
+                foreach (Transform child in transform)
+                {
+                    Instantiate(item, inventoryTest.slots[i].transform, false);
+                }
+
+                break;
+            }
+        }
+    }
+}
